b4e39e5162407ae0692c39265b31f815d80486e7
Eslam Adel <eslam.eng.dev@gmail.com>
2026-03-01 14:37:13 +0200

    refactor(RootLayout): reorganize navigation stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,34 +1,24 @@
-// css exports
+// css imports
 import "@/global.css";
-// react native exports
+// react native imports
 import { SafeAreaView } from "react-native-safe-area-context";
-// expo export
+// expo imports
 import { Stack } from "expo-router";
 // toast imports
+import UserContext from "@/context/userContext";
 import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
   return (
-    <SafeAreaView className="flex-1">
-      <Toast position="bottom" bottomOffset={20} />
-      <Stack
-        screenOptions={{ headerShown: false, presentation: "transparentModal" }}
-      >
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="(onboarding)/onboarding" />
-        <Stack.Screen name="(authScreens)/login" />
-        <Stack.Screen name="(authScreens)/register" />
-        <Stack.Screen name="(AppScreens)/property/[id]" />
-        <Stack.Screen name="(AppScreens)/chat/index" />
-        <Stack.Screen name="(AppScreens)/chat/[chatId]" />
-        <Stack.Screen name="(AppScreens)/notifications" />
-        <Stack.Screen
-          name="(authScreens)/forgot-password"
-          options={{
-            presentation: "formSheet",
-          }}
-        />
-      </Stack>
-    </SafeAreaView>
+    <UserContext>
+      <SafeAreaView className="flex-1">
+        <Toast position="bottom" bottomOffset={20} />
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(AppScreens)" />
+          <Stack.Screen name="(authScreens)" />
+          <Stack.Screen name="onboarding" />
+        </Stack>
+      </SafeAreaView>
+    </UserContext>
   );
 }
