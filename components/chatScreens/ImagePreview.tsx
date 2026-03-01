bf497667a8ccd580a49db5f501f09d6491b3c783
Eslam Adel <eslam.eng.dev@gmail.com>
2026-03-01 15:30:40 +0200

    feat(components): use useImageContext hook in ImagePreview.tsx

--- components/chatScreens/ImagePreview.tsx
+++ components/chatScreens/ImagePreview.tsx
@@ -1,13 +1,15 @@
 // react imports
 import { Modal, TouchableWithoutFeedback, View } from "react-native";
-// types imports
-import { ImagePreviewProps } from "@/types/type";
+// context
+import { useImageContext } from "@/context/imageContext";
 // imageZoom imports
 import { ImageZoom } from "@likashefqet/react-native-image-zoom";
 //  gesture handler imports
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-const ImagePreview = ({ visible, onClose }: ImagePreviewProps) => {
+const ImagePreview = () => {
+  const { visible, uri, hideImage } = useImageContext();
+
   // test rerender
   console.warn("Image Preview Rendered");
 
@@ -16,16 +18,18 @@ const ImagePreview = ({ visible, onClose }: ImagePreviewProps) => {
       visible={visible}
       transparent
       animationType="slide"
-      onRequestClose={onClose}
+      onRequestClose={hideImage}
     >
-      <TouchableWithoutFeedback onPress={onClose}>
+      <TouchableWithoutFeedback onPress={hideImage}>
         <GestureHandlerRootView>
           <View className="flex-1 justify-center items-center bg-black/50">
-            <ImageZoom
-              uri="https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg"
-              style={{ width: "100%", height: "100%", zIndex: 10 }}
-              doubleTapScale={3}
-            />
+            {uri && (
+              <ImageZoom
+                uri={uri}
+                style={{ width: "100%", height: "100%", zIndex: 10 }}
+                doubleTapScale={3}
+              />
+            )}
           </View>
         </GestureHandlerRootView>
       </TouchableWithoutFeedback>
