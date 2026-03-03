c5016d4200fb0aee6f1dec792e7cc0970954cdd6
Eslam Adel <eslam.eng.dev@gmail.com>
2026-03-03 14:40:50 +0200

    refactor(layout): remove unnecessary code from Root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,35 +4,22 @@ import "@/global.css";
 import { SafeAreaView } from "react-native-safe-area-context";
 // expo imports
 import { Stack } from "expo-router";
-// navigation react imports
-import { NavigationContainer } from "@react-navigation/native";
 // toast imports
+import Toast from "react-native-toast-message";
+// context imports
 import ImageContext from "@/context/imageContext";
 import UserContext from "@/context/userContext";
-import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
-  const linking = {
-    prefixes: ["housely://"],
-    config: {
-      screens: {
-        Home: "",
-        Property: "property/:id",
-      },
-    },
-  };
-
   return (
     <UserContext>
       <ImageContext>
         <SafeAreaView className="flex-1">
           <Toast position="bottom" bottomOffset={20} />
           <Stack screenOptions={{ headerShown: false }}>
-            <NavigationContainer linking={linking}>
-              <Stack.Screen name="(AppScreens)" />
-              <Stack.Screen name="(authScreens)" />
-              <Stack.Screen name="onboarding" />
-            </NavigationContainer>
+            <Stack.Screen name="(AppScreens)" />
+            <Stack.Screen name="(authScreens)" />
+            <Stack.Screen name="onboarding" />
           </Stack>
         </SafeAreaView>
       </ImageContext>
