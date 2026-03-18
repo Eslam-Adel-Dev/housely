2aca68001e252c2b13e9b6b3694368722da94a09
Eslam Adel <eslam.eng.dev@gmail.com>
2026-03-18 17:50:55 +0200

    feat(components): add SearchProps to SearchComp

--- components/SearchComp.tsx
+++ components/SearchComp.tsx
@@ -3,11 +3,12 @@ import { TouchableOpacity, View } from "react-native";
 // icons imports
 import Filter from "@/assets/icons/Filter.svg";
 import Search from "@/assets/icons/Search.svg";
+import { SearchProps } from "@/types/type";
 import { Input } from "./ui/input";
 
-const SearchComp = () => {
+const SearchComp = ({ handleOpenFilterModal }: SearchProps) => {
   return (
-    <View className="w-full border-2 border-zinc-300/50 rounded-2xl px-4 py-2 flex-row items-center justify-between">
+    <View className="w-full border-2 border-zinc-300/50 rounded-2xl px-4 py-2 flex-row items-center justify-between bg-white">
       <Search size={20} />
       <Input
         keyboardType="default"
@@ -16,7 +17,7 @@ const SearchComp = () => {
         placeholder="Search Here..."
         className="border-0 shadow-none outline-none flex-1 placeholder:text-zinc-400 text-zinc-600"
       />
-      <TouchableOpacity>
+      <TouchableOpacity onPress={handleOpenFilterModal}>
         <Filter size={20} />
       </TouchableOpacity>
     </View>
