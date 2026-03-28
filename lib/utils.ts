5edd3dfb26f98d46d3109465c534923ee817ca2c
Eslam Adel <eslam.eng.dev@gmail.com>
2026-03-28 09:31:09 +0200

    feat(utils): add formatCardNumber, formatExpiry, and getCardLogo

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,11 @@
-// lib/utils.ts
-import { Review } from "@/types/type";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+// images imports
+import AmexLogo from "../assets/images/American-Express-logo.png";
+import MastercardLogo from "../assets/images/Mastercard-Logo.png";
+import VisaLogo from "../assets/images/Visa-Logo.png";
+// types imports
+import { Review } from "@/types/type";
 
 //=============================================
 
@@ -34,3 +38,39 @@ export const formatChatTime = (messageTime: string | number | Date): string => {
     ? currentFormattedTime
     : `${date.toLocaleDateString()} ${currentFormattedTime}`;
 };
+
+//=============================================
+
+export const formatCardNumber = (num: string) => {
+  const clean = num.replace(/\D/g, "");
+  const groups = clean.match(/.{1,4}/g);
+  if (!groups) return "";
+  return groups.join(" ").slice(0, 19);
+};
+
+//=============================================
+
+export const formatExpiry = (expiry: string) => {
+  const clean = expiry.replace(/\D/g, "");
+  if (clean.length > 2) {
+    return `${clean.slice(0, 2)}/${clean.slice(2, 4)}`;
+  }
+  return clean;
+};
+
+//=============================================
+
+export const getCardLogo = (number: string) => {
+  const cleanNum = number.replace(/\s/g, "");
+  if (cleanNum.startsWith("4")) {
+    return VisaLogo;
+  }
+  if (/^(5[1-5]|222[1-9]|22[3-9]|2[3-6]|27[01]|2720)/.test(cleanNum)) {
+    return MastercardLogo;
+  }
+  if (/^3[47]/.test(cleanNum)) {
+    return AmexLogo;
+  }
+  return null;
+};
+
