f89f1cb7d54c312a5121dec7bb0bee5415175e06
Eslam Adel <eslam.eng.dev@gmail.com>
2026-04-15 01:08:52 +0200

    feat(api): setup API method wrappers for GET, POST, PUT, DELETE

---
+++ api/axios/methods.ts
@@ -0,0 +1,57 @@
+import { AxiosRequestConfig } from "axios";
+import axiosInstance from "./axiosApi";
+
+//=========================================
+// GET request
+export const getApi = async (url: string, options?: AxiosRequestConfig) => {
+  try {
+    const response = await axiosInstance.get(url, options);
+    return response;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+//=========================================
+// POST request
+
+export const postApi = async (
+  url: string,
+  data: any,
+  options?: AxiosRequestConfig,
+) => {
+  try {
+    const response = await axiosInstance.post(url, data, options);
+    return response;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+//=========================================
+// PUT request
+
+export const putApi = async (
+  url: string,
+  data: any,
+  options?: AxiosRequestConfig,
+) => {
+  try {
+    const response = await axiosInstance.put(url, data, options);
+    return response;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+//=========================================
+// DELETE request
+
+export const deleteApi = async (url: string, options?: AxiosRequestConfig) => {
+  try {
+    const response = await axiosInstance.delete(url, options);
+    return response;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
