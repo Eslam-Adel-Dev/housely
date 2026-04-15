be882a4a9aa8c4c79f1198e82b5052da41f012d2
Eslam Adel <eslam.eng.dev@gmail.com>
2026-04-15 02:20:28 +0200

    refactor(folder): move useOnboardingStatus.tsx to hooks/useMmkvStorage

--- hooks/useMmkvStorage/useOnboardingStatus.tsx
+++ hooks/useMmkvStorage/useOnboardingStatus.tsx
@@ -1,6 +1,7 @@
-// hooks/useOnboardingStatus.ts
 import { storage } from "@/lib/mmkvStorage";
 import { useEffect, useState } from "react";
+
+// =================================================================
 
 export const useOnboardingStatus = () => {
   const [isOnboardingCompleted, setIsOnboardingCompleted] = useState(false);
