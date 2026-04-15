0d40e10763df9e0e3267f4c2615bf47ee50547b4
Eslam Adel <eslam.eng.dev@gmail.com>
2026-04-15 02:06:28 +0200

    feat(auth): integrate useLogin API hook into Login screen UI

--- app/(authScreens)/login.tsx
+++ app/(authScreens)/login.tsx
@@ -7,14 +7,12 @@ import ScreenWrapper from "@/components/ScreenWrapper";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 // expo imports
-import { Link, useRouter } from "expo-router";
+import { Link } from "expo-router";
 // react native imports
 import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 //images imports
 import facebook_logo from "@/assets/images/facebook-logo.png";
 import google_logo from "@/assets/images/google-logo.png";
-// context imports
-import { useUserContext } from "@/context/userContext";
 // types import
 import { loginInput } from "@/types/type";
 // react hook form
@@ -22,8 +20,8 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { Controller, useForm } from "react-hook-form";
 // yup schemas
 import { loginSchema } from "@/lib/yupSchemas/loginSchema";
-// toast imports
-import Toast from "react-native-toast-message";
+// hooks imports
+import { useLogin } from "@/api/hooks/useAuth";
 
 //=========================================================
 
@@ -49,23 +47,16 @@ const Login = () => {
   });
   const [focusedPassword, setFocusedPassword] = useState(false);
   const [focusedEmail, setFocusedEmail] = useState(false);
-  const { login } = useUserContext();
-  const router = useRouter();
+  const { mutateLogin, isPending, isSuccess, isError } = useLogin();
 
   // -----------------------
 
   const handleLogin = async (data: loginInput) => {
+    console.log(data);
     try {
-      console.log(data);
-      login();
-      Toast.show({
-        type: "success",
-        text1: "Welcome back! 👋",
-        text2: "You have successfully signed in.",
-      });
-      router.replace("/");
+      mutateLogin(data);
     } catch (error) {
-      console.error(error);
+      console.log("FETCH ERROR", error);
     }
   };
 
@@ -159,6 +150,7 @@ const Login = () => {
               control={control}
               render={({ field: { onChange, value } }) => (
                 <CheckboxWithLabel
+                  classNameContainer=""
                   label="Remember Me"
                   classNameLabel="text-md font-semibold"
                   checked={value}
@@ -176,6 +168,8 @@ const Login = () => {
             onButtonPress={handleSubmit(handleLogin) as () => void}
             textClassName="text-white"
             className="rounded-lg"
+            loading={isPending}
+            disabled={isPending}
           >
             Sign In
           </CustomButton>
