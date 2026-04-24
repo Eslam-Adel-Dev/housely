84400ce291cca7a779056781c61cae98a97c0b59
Eslam Adel <eslam.eng.dev@gmail.com>
2026-04-25 01:53:48 +0300

    feat(hooks): rewrite useFavoriteProperties to handle fetching and adding new property

--- hooks/useFavoriteProperties.tsx
+++ hooks/useFavoriteProperties.tsx
@@ -1,44 +1,35 @@
-// context imports
-import { useUserContext } from "@/context/userContext";
-// types imports
-import { Properties, Property } from "@/types/type";
-// react imports
-import { useCallback, useMemo } from "react";
-// toast imports
-import Toast from "react-native-toast-message";
+import { useAddFavorite, useFetchFavorites } from "@/api/hooks/useProperties";
+import { Property } from "@/types/type";
+import { useCallback } from "react";
 
-const useFavoriteProperties = (property: Property) => {
-  const { favorites, setFavorites } = useUserContext();
-  const id = property?._id;
+//================================================================
 
-  const isLiked = useMemo(
-    () => favorites.some((likedProperty: Property) => likedProperty._id === id),
-    [favorites, id],
+export const useFavoriteProperties = () => {
+  const {
+    favorites,
+    refreshFavorites,
+    isFavoritesFetched,
+    isFavoritesPending,
+    isFavoritesSuccess,
+    isFavoritesError,
+  } = useFetchFavorites();
+
+  const { mutate } = useAddFavorite();
+
+  const toggleLike = useCallback(
+    async (property: Property, action: "add" | "remove") => {
+      mutate({ property, action });
+    },
+    [mutate],
   );
 
-  const toggleLike = useCallback(() => {
-    setFavorites((prevFavorites: Properties) => {
-      const exists = prevFavorites?.some((p: Property) => p._id === id);
-
-      if (exists) {
-        Toast.show({
-          type: "info",
-          text1: "Removed from favorites",
-          text2: "Property has been removed from your favorites.",
-        });
-        return prevFavorites?.filter((p) => p._id !== id);
-      }
-
-      Toast.show({
-        type: "success",
-        text1: "Added to favorites ❤️",
-        text2: "Property has been saved to your favorites.",
-      });
-      return [...prevFavorites, property];
-    });
-  }, [id, setFavorites, property]);
-
-  return { isLiked, toggleLike };
+  return {
+    favorites: favorites ?? [],
+    refreshFavorites,
+    isFavoritesFetched,
+    isFavoritesPending,
+    isFavoritesSuccess,
+    isFavoritesError,
+    toggleLike,
+  };
 };
-
-export default useFavoriteProperties;
