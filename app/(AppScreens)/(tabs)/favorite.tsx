15df2bda47b9b51545dabd9aa094acad943340a8
Eslam Adel <eslam.eng.dev@gmail.com>
2026-04-25 02:48:00 +0300

    feat(screens): use api data, refresh control, and skeleton for pending state in favorites screen

--- app/(AppScreens)/(tabs)/favorite.tsx
+++ app/(AppScreens)/(tabs)/favorite.tsx
@@ -1,52 +1,68 @@
 // react native imports
-import { View } from "react-native";
+import { RefreshControl, View } from "react-native";
 // components imports
 import NoFavoriteImage from "@/assets/images/NoLocation.svg";
 import PropertyCard2 from "@/components/homeScreen/PropertyCard2";
 import TitleBar from "@/components/layout/TitleBar";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import EmptyState from "@/components/ui/EmptyState";
-// context imports
-import { useUserContext } from "@/context/userContext";
 // flashlist imports
 import { FlashList } from "@shopify/flash-list";
-// types imports
+// hooks imports
+import { useFavoriteProperties } from "@/hooks/useFavoriteProperties";
 import { Property } from "@/types/type";
+// skeletons imports
+import PropertyCard2Skeleton from "@/components/skeletons/PropertyCard2Skeleton";
 
 //===================================================================
 
 const Favorite = () => {
-  const { favorites } = useUserContext() as { favorites: Property[] };
+  const { favorites, isFavoritesPending, refreshFavorites } =
+    useFavoriteProperties();
 
-  // ui part
   return (
     <ScreenWrapper>
       <TitleBar title="Favorite" />
-      {/* --------------------------------------- */}
 
-      <FlashList
-        data={favorites}
-        renderItem={({ item }) => (
-          <PropertyCard2 {...item} image={item.images[0]} fullWidth />
-        )}
-        horizontal={false}
-        keyExtractor={(item) => item.id.toString()}
-        ItemSeparatorComponent={() => <View className="w-4 h-4" />}
-        showsHorizontalScrollIndicator={false}
-        contentContainerClassName="py-10"
-        contentContainerStyle={
-          favorites.length === 0
-            ? { flex: 1, justifyContent: "center", alignItems: "center" }
-            : {}
-        }
-        ListEmptyComponent={
-          <EmptyState
-            ImageComp={NoFavoriteImage}
-            title="You have no favorites yet"
-            subTitle="Start exploring and save your favorite properties here!"
-          />
-        }
-      />
+      {isFavoritesPending ? (
+        <View className="flex-1 mt-5">
+          {[...Array(10)].map((_, index) => (
+            <View key={index} className="mb-4">
+              <PropertyCard2Skeleton fullWidth={true} />
+            </View>
+          ))}
+        </View>
+      ) : (
+        <FlashList
+          data={favorites}
+          renderItem={({ item }: { item: Property }) => (
+            <PropertyCard2 {...item} image={item?.images?.[0]} fullWidth />
+          )}
+          horizontal={false}
+          keyExtractor={(item) => item._id.toString()}
+          ItemSeparatorComponent={() => <View className="w-4 h-4" />}
+          showsHorizontalScrollIndicator={false}
+          contentContainerClassName="py-10"
+          contentContainerStyle={
+            favorites?.length === 0
+              ? { flex: 1, justifyContent: "center", alignItems: "center" }
+              : {}
+          }
+          ListEmptyComponent={
+            <EmptyState
+              ImageComp={NoFavoriteImage}
+              title="You have no favorites yet"
+              subTitle="Start exploring and save your favorite properties here!"
+            />
+          }
+          refreshControl={
+            <RefreshControl
+              refreshing={isFavoritesPending}
+              onRefresh={refreshFavorites}
+            />
+          }
+        />
+      )}
     </ScreenWrapper>
   );
 };
