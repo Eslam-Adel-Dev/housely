6b34f64cf7f8bb6712caec21102f0d992e75ef59
Eslam Adel <eslam.eng.dev@gmail.com>
2026-05-11 05:45:50 +0300

    feat(components): add reusable RedDot UI component

---
+++ components/ui/RedDot.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+const RedDot = () => {
+  return (
+    <View className="absolute top-0 right-0 bg-red-500 rounded-full size-3 z-10" />
+  );
+};
+
+export default RedDot;
