7cef2fa424123fcee1de2af8e1e11156f42749b4
Eslam Adel <eslam.eng.dev@gmail.com>
2026-05-11 03:22:12 +0300

    feat(api): implement useSetUserLocation hook for updating user coordinates

--- api/hooks/useUser.ts
+++ api/hooks/useUser.ts
@@ -4,6 +4,43 @@ import { usePatchHook, usePostHook } from "./useMethods";
 
 //================================================================
 
+interface Location {
+  type: "Point";
+  coordinates: [number, number];
+}
+
+export const useSetUserLocation = () => {
+  const { updateUser } = useUserStore();
+  const { mutate, isPending, isError, isSuccess } = usePostHook(
+    ENDPOINTS.USER.LOCATION,
+  );
+
+  const mutateLocation = (data: any) => {
+    mutate(data, {
+      onSuccess: (data) => {
+        const { longitude, latitude } = data?.data?.coordinates;
+        const location: Location = {
+          type: "Point",
+          coordinates: [longitude, latitude],
+        };
+
+        console.log("LOCATION FOR STORE", location);
+        updateUser({ location });
+        console.log("USER LOCATION", data);
+      },
+      onError: (error) => {
+        console.log("USER LOCATION ERROR", error);
+      },
+    });
+  };
+
+  return { mutateLocation, isPending, isError, isSuccess };
+};
+
+//================================================================
+
+//================================================================
+
 export const useUserProfileImage = () => {
   const { updateUser } = useUserStore();
   const { mutate, isPending, isError, isSuccess } = usePatchHook(
